508901453894ab7037cacd70a64a9f0384c209f8
bhartivishwakarma <bhartiv12301@gmail.com.com>
2025-09-09 14:23:27 +0530

    Update translation examples in template3.py and template5.py

--- template5.py
+++ template5.py
@@ -1,3 +1,7 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 llm=OllamaLLM(model="gemma:2b",temperature=0)
+
+translate_prompt=ChatPromptTemplate.from_template("Translate the following from{from_language} to {to_language}.Provide only the translated text:{content}")
+prompt=translate_prompt.invoke({"from_language":"English","to_language":"French","content":"I love programming"})
+print(llm.invoke(prompt))
