17074a2736af89c7b7ece5386978493975aaba50
bhartivishwakarma <bhartiv12301@gmail.com.com>
2025-09-11 22:12:46 +0530

    Add initial implementation of langchain Runnables with OllamaLLM for question answering

---
+++ chains/runnables.py
@@ -0,0 +1,15 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+llm=OllamaLLM(model="gemma:2b",base_url="http://localhost:11434",temperature=0,streaming=False)
+
+#langchain Runnables
+template=PromptTemplate.from_template("Answer the following Question:{question}")
+prompt=template.invoke({"question":"In which country is Nvidia's world headquater?"})
+print(llm.invoke(prompt))
+questions=[{"question":"In which country is Nvidia's world headquater?"},
+           {"question":"When was Nvidia founded?"},
+           {"question":"Who is the CEO of Nvidia?"}]
+prompts=template.batch(questions)
+print(prompts)
